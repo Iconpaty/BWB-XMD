2f4627bec98c66f3fed9fac50a4775af8ce3fedf
Iconpaty <164565508+Iconpaty@users.noreply.github.com>
2025-07-08 10:39:09 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "BWB XMD",
   botOwner: 'Princetech', // Your name
-  ownerNumber: '255763834140', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255762366615', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
